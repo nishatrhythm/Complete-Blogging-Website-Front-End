f7872231ed989fc7359b58a28989820dcaa10632
nishatrhythm <66105148+nishatrhythm@users.noreply.github.com>
2023-04-28 16:15:05 +0600

    Design enhancement

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 const navItems = document.querySelector('.nav__items');
+const openNavBtn = document.querySelector('#open__nav-btn');
+const closeNavBtn = document.querySelector('#close__nav-btn');
+
+// opens nav dropdown
+const openNav = () => {
+    navItems.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block'
+}
+
+// closes nav dropdown
+const closeNav = () => {
+    navItems.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none'
+}
+
+openNavBtn.addEventListener('click', openNav);
+closeNavBtn.addEventListener('click', closeNav);
