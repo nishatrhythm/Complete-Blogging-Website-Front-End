a6b83454cb5c63825f81fc060f7d3ed65a4c995a
nishatrhythm <66105148+nishatrhythm@users.noreply.github.com>
2023-04-28 22:37:50 +0600

    Design enhancement

--- main.js
+++ main.js
@@ -18,3 +18,25 @@ const closeNav = () => {
 
 openNavBtn.addEventListener('click', openNav);
 closeNavBtn.addEventListener('click', closeNav);
+
+// sidebar for small devices
+const sidebar = document.querySelector('aside');
+const showSidebarBtn = document.querySelector('#show__sidebar-btn');
+const hideSidebarBtn = document.querySelector('#hide__sidebar-btn');
+
+// shows sidebar on small devices
+const showSidebar = () => {
+    sidebar.style.left = '0';
+    showSidebarBtn.style.display = 'none';
+    hideSidebarBtn.style.display = 'inline-block';
+}
+
+// hides sidebar on small devices
+const hideSidebar = () => {
+    sidebar.style.left = '-100%';
+    showSidebarBtn.style.display = 'inline-block';
+    hideSidebarBtn.style.display = 'none';
+}
+
+showSidebarBtn.addEventListener('click', showSidebar);
+hideSidebarBtn.addEventListener('click', hideSidebar);
